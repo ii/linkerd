1b3a8a20cf8875690b42435c7aaa2022d2838608
Eliza Weisman <eliza@buoyant.io>
2017-07-26 09:39:31 -0500

    Add time unit param to StreamStatsFilter

--- router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/StreamStatsFilter.scala
@@ -11,9 +11,9 @@ import com.twitter.util._
 
 object StreamStatsFilter {
   /**
-   * Configures a [[StreamStatsFilter.module]] to track latency using the
-   * given [[TimeUnit]].
-   */
+  * Configures a [[StreamStatsFilter.module]] to track latency using the
+  * given [[TimeUnit]].
+  */
   case class Param(unit: TimeUnit) {
     def mk(): (Param, Stack.Param[Param]) = (this, Param.param)
   }
