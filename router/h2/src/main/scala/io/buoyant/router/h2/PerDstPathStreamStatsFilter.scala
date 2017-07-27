54cecd96eabff4d3ac54e07d3dfdf3d11d86bfc4
Eliza Weisman <eliza@buoyant.io>
2017-07-27 11:13:53 -0500

    Add ExceptionStatsHandler to LocalClassifier & PerDstPath stream stats

--- router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
+++ router/h2/src/main/scala/io/buoyant/router/h2/PerDstPathStreamStatsFilter.scala
@@ -21,6 +21,7 @@ object PerDstPathStreamStatsFilter {
 
       override def make(
         statsP: param.Stats,
+        exHandlerP: param.ExceptionStatsHandler,
         statsFilterP: StreamStatsFilter.Param,
         next: ServiceFactory[Request, Response]
       ): ServiceFactory[Request, Response] =
@@ -29,14 +30,15 @@ object PerDstPathStreamStatsFilter {
             val StreamStatsFilter.Param(timeUnit) = statsFilterP
             val H2StreamClassifier(classifier) =
               StreamClassifierCtx.current.getOrElse(H2StreamClassifier.param.default)
+            val param.ExceptionStatsHandler(exHandler) = exHandlerP
 
-            def mkScopedStatsFilter(path: Path): Filter[Request, Response, Request, Response] = {
+            def mkScopedStatsFilter(path: Path): SimpleFilter[Request, Response] = {
               val name = path.show.stripPrefix("/")
               val scopedStats = stats.scope("service", name)
-              new StreamStatsFilter(scopedStats, classifier, timeUnit)
+              new StreamStatsFilter(scopedStats, classifier, exHandler, timeUnit)
             }
 
-            val filter = new PerDstPathFilter(mkScopedStatsFilter _)
+            val filter = new PerDstPathFilter(mkScopedStatsFilter)
             filter.andThen(next)
 
           // if the stats receiver is the `NullReceiver`, don't make a filter.
