0b2bdc1cdfc17a6edd962866c02fcde5a0d098d7
Eliza Weisman <eliza@buoyant.io>
2017-08-12 12:09:37 -0500

    Remove dead code

--- k8s/src/main/scala/io/buoyant/k8s/EndpointsNamer.scala
+++ k8s/src/main/scala/io/buoyant/k8s/EndpointsNamer.scala
@@ -125,18 +125,6 @@ abstract class EndpointsNamer(
   val cache = new EndpointsCache
   protected[this] val variablePrefixLength: Int
 
-
-  //  private[k8s] val serviceNs = new Ns[
-  //    v1.Service,
-  //    v1.ServiceWatch,
-  //    v1.ServiceList,
-  //    PortCache
-  //    ](backoff, timer) {
-  //    override protected def mkResource(name: String) = mkApi(name).services
-  //
-  //    override protected def mkCache(name: String) = new PortCache()
-  //  }
-
   @inline
   private[this] def mkNameTree(id: Path, residual: Path)
     (maybeAddrs: Option[Var[Addr]]): Activity.State[NameTree[Name]] =
@@ -144,7 +132,6 @@ abstract class EndpointsNamer(
       case Some(addrs) => Activity.Ok(NameTree.Leaf(Name.Bound(addrs, idPrefix ++ id, residual)))
       case None => Activity.Ok(NameTree.Neg)
     }
-
 
   private[k8s] def lookupServices(
     nsName: String,
@@ -159,8 +146,6 @@ abstract class EndpointsNamer(
     val nameTree = mkNameTree(id, residual) _
 
     @inline def initCache(endpoints: v1.Endpoints): EndpointsCache = {
-      // wish i didn't have to call initialize then get here, just to get the addrs
-      // from the Endpoints object. who wrote this api anyway? /me raises hand. whoops.
       cache.initialize(endpoints)
       cache
     }
@@ -178,7 +163,6 @@ abstract class EndpointsNamer(
       mkApi(nsName)
         .endpoints(serviceName)
         .activity(initCache, labelSelector = labelSelector) { (_, event) =>
-          // and similarly update then get is not great programming either
           cache.update(event)
           cache
         }
@@ -218,16 +202,14 @@ object EndpointsNamer {
   protected case class Svc(endpoints: Set[Endpoint], ports: PortMap)
 
   protected[EndpointsNamer] case class SvcCache(name: String, init: Svc) {
+
     /**
      * For a given port number, apply the port mapping of the service.  The target port of the port
      * mapping may be a named port and the named port may or may not exist.  The outer Var[Option]
      * of the return type tracks whether the port exists and the inner Var[Addr] tracks the actual
      * endpoints if the port does exist.
      */
-    def lookupNumberedPort(
-      mappings: PortMappingCache,
-      portNumber: Int
-    ): Var[Option[Var[Addr]]] =
+    def lookupNumberedPort(mappings: PortMappingCache, portNumber: Int): Var[Option[Var[Addr]]] =
       mappings(portNumber)
         .map { targetPort =>
           // target may be an int (port number) or string (port name)
@@ -310,15 +292,9 @@ object EndpointsNamer {
 
   class EndpointsCache extends Ns.ObjectCache[v1.Endpoints, v1.EndpointsWatch] {
 
-    import EndpointsCache._
-
     private[this] val state = Var[Activity.State[Map[String, SvcCache]]](Activity.Pending)
-    private[this] var cache = Map.empty[CacheKey, VarUp[Addr]]
-
     val services: Activity[Map[String, SvcCache]] = Activity(state)
 
-    def getNumberedPort(nsName: String, serviceName: String, portNumber: Int): Option[VarUp[Addr]]
-    = ???
 
     def initialize(endpoints: v1.Endpoints): Unit =
       add(endpoints)
@@ -331,97 +307,43 @@ object EndpointsNamer {
         case v1.EndpointsDeleted(endpoints) => delete(endpoints)
       }
 
-    def get(nsName: String, portName: String, serviceName: String): Option[Var[Addr]] =
-      cache.get(CacheKey(nsName, portName, serviceName))
-
-    private[this] def add(endpoints: v1.Endpoints): Unit = {
-      val (endpointsMap, portsMap) = toMap(endpoints)
-      for {(key, addr) <- endpointsMap} synchronized {
-        log.debug("k8s ns %s added service: %s; port: %s", key.nsName, key.serviceName, key.portName)
-        cache += key -> Var(addr)
-      }
-      for {svc: SvcCache <- endpoints} synchronized {
+    private[this] def add(endpoints: v1.Endpoints): Unit =
+      for {svc <- endpoints} synchronized {
+        log.debug(s"k8s added: $svc")
         val svcs = state.sample() match {
           case Activity.Ok(svcs) => svcs
           case _ => Map.empty[String, SvcCache]
         }
         state() = Activity.Ok(svcs + (svc.name -> svc))
       }
-    }
 
-
-    private[this] def modify(endpoints: v1.Endpoints): Unit = {
-      val (endpointsMap, portsMap) = toMap(endpoints)
-      for {(key, modified) <- endpointsMap} synchronized {
-        cache.get(key) match {
-          case Some(addr) =>
-            log.info("k8s ns %s modified service: %s; port %s", key.nsName, key.serviceName, key.portName)
-            addr.update(modified)
-          case None =>
-            log.info(
-              s"k8s ns ${key.nsName} added service ${key.serviceName}; port ${key.portName}")
-            cache += key -> Var(modified)
+    private[this] def modify(endpoints: v1.Endpoints): Unit =
+      for {name <- endpoints.getName} synchronized {
+        log.debug(s"k8s modified: $name")
+        state.sample() match {
+          case Activity.Ok(snap) =>
+            snap.get(name) match {
+              case None =>
+                log.warning(s"k8s ns received modified watch for unknown service $name")
+              case Some(svc) =>
+                svc() = endpoints.subsets
+            }
+          case _ =>
         }
       }
-    }
 
-    private[this] def delete(endpoints: v1.Endpoints): Unit = {
-      val (endpointsMap, portsMap) = toMap(endpoints)
-      for {(key, _) <- endpointsMap} synchronized {
-        cache.get(key) match {
-          case Some(addr) => addr.update(Addr.Neg)
-          case None =>
-            log.warning(
-              "k8s ns %s received delete watch for unknown service %s; port %s",
-              key.nsName, key.serviceName, key.portName
-            )
+    private[this] def delete(endpoints: v1.Endpoints): Unit =
+      for {name <- endpoints.getName} synchronized {
+        log.debug(s"k8s deleted: $name")
+        state.sample() match {
+          case Activity.Ok(snap) =>
+            for (svc <- snap.get(name)) {
+              state() = Activity.Ok(snap - name)
+            }
+
+          case _ =>
         }
       }
-    }
-  }
-
-  object EndpointsCache {
-
-    private case class PortAddr(portName: String, addr: Address)
-
-    private case class CacheKey(nsName: String, portName: String, serviceName: String)
-
-    /**
-     * Convert a [[v1.Endpoints]] object to a map of `(namespace, port, service) -> Address`
-     *
-     * @param endpoints
-     * @return
-     */
-    private def toMap(endpoints: v1.Endpoints): (Map[CacheKey, Addr], Map[Int, String]) = {
-      val endpointMap = mutable.Map.empty[CacheKey, Addr]
-      val portNamesMap = mutable.Map.empty[Int, String]
-      for {
-        metadata <- endpoints.metadata
-        namespace <- metadata.namespace
-        name <- metadata.name
-      } {
-        val portsAndAddrs = for {
-          subset <- endpoints.subsetsSeq
-          address <- subset.addressesSeq
-          port <- subset.portsSeq
-          portName <- port.name
-          portNum = port.port
-        } yield {
-          portNamesMap(portNum) = portName
-          PortAddr(portName, Address(address.ip, portNum))
-        }
-        portsAndAddrs
-          .groupBy(_.portName)
-          .mapValues {
-            _.map(_.addr)
-          }
-          .foreach { case (portName, addresses) =>
-            val key = CacheKey(namespace, portName, name)
-            endpointMap(key) = Addr.Bound(addresses: _*)
-          }
-      }
-      (endpointMap.toMap, portNamesMap.toMap)
-    }
   }
 
 }
