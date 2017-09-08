ebd90330e388e573724f1151257fb354d91cd2bc
dlx <denver@ii.coop>
2017-09-09 11:16:53 +1200

    Fix Syntax

--- project/Base.scala
+++ project/Base.scala
@@ -172,8 +172,7 @@ class Base extends Build {
     dockerTag += (dockerTag in Global).or( initialize[String] { _ =>
       s"$version-$configuration"
     }).value,
-    dockerRegistry += (dockerRegistry in Global)
-    }).value,
+    dockerRegistry += (dockerRegistry in Global).value,
     imageNames in docker := Seq(ImageName(
       namespace = Some(dockerRegistry.value),
       repository = name.value,
